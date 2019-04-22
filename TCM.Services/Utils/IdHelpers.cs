016f5734fa045bfd783271c309cc5f37afe98a20
mfregoso <mfregoso@users.noreply.github.com>
2019-04-21 17:50:35 -0700

    initial project setup

---
+++ TCM.Services/Utils/IdHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TCM.Services.Utils
+{
+    public class IdHelpers
+    {
+        public static bool IsValid(string id)
+        {
+            var numChk = new Regex("^[0-9]{1,8}$");
+            if (!numChk.IsMatch(id)) return false;
+            int clubId = Int32.Parse(id);
+            return clubId > 0 ? true : false;
+        }
+
+        public static string FormatId(string id)
+        {
+            if (id.Length == 8) return id;
+            string formattedId = id.PadLeft(8).Replace(" ", "0");
+            return formattedId;
+        }
+    }
+}
---
+++ TCM.Web/Controllers/ClubsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TCM.Services.Utils;
+
+namespace TCM.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClubsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "please get by id: e.g. clubs/455";
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> GetByID(string id)
+        {
+            bool validId = IdHelpers.IsValid(id);
+            if (validId) return IdHelpers.FormatId(id);
+            return id + " is NOT valid!";
+        }
+    }
+}
