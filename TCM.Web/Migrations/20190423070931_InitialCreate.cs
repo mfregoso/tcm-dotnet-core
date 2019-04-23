6449293ec98baecef0b776d3cc257bb1d1b9abf1
mfregoso <mfregoso@users.noreply.github.com>
2019-04-23 00:42:32 -0700

    post-initial create

---
+++ TCM.Web/Migrations/20190423070931_InitialCreate.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TCM.Web.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clubs",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Exists = table.Column<bool>(nullable: false),
+                    MembershipCount = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clubs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MetricsHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MonthEnd = table.Column<string>(nullable: true),
+                    Members = table.Column<int>(nullable: true),
+                    Goals = table.Column<int>(nullable: true),
+                    ClubId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MetricsHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MetricsHistory_Clubs_ClubId",
+                        column: x => x.ClubId,
+                        principalTable: "Clubs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MetricsHistory_ClubId",
+                table: "MetricsHistory",
+                column: "ClubId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MetricsHistory");
+
+            migrationBuilder.DropTable(
+                name: "Clubs");
+        }
+    }
+}
