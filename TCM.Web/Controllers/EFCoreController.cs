b215c351fa52b0bce24867fe443a58b8408a623d
mfregoso <mfregoso@users.noreply.github.com>
2019-04-23 21:16:15 -0700

    auto-generate CRUD EF core controller

---
+++ TCM.Web/Controllers/EFCoreController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TCM.Web.Models;
+
+namespace TCM.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EFCoreController : ControllerBase
+    {
+        private readonly ClubDataContext _context;
+
+        public EFCoreController(ClubDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/EFCore
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Club>>> GetClubs()
+        {
+            return await _context.Clubs.ToListAsync();
+        }
+
+        // GET: api/EFCore/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Club>> GetClub(string id)
+        {
+            var club = await _context.Clubs.FindAsync(id);
+
+            if (club == null)
+            {
+                return NotFound();
+            }
+
+            return club;
+        }
+
+        // PUT: api/EFCore/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClub(string id, Club club)
+        {
+            if (id != club.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(club).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClubExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/EFCore
+        [HttpPost]
+        public async Task<ActionResult<Club>> PostClub(Club club)
+        {
+            _context.Clubs.Add(club);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetClub", new { id = club.Id }, club);
+        }
+
+        // DELETE: api/EFCore/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Club>> DeleteClub(string id)
+        {
+            var club = await _context.Clubs.FindAsync(id);
+            if (club == null)
+            {
+                return NotFound();
+            }
+
+            _context.Clubs.Remove(club);
+            await _context.SaveChangesAsync();
+
+            return club;
+        }
+
+        private bool ClubExists(string id)
+        {
+            return _context.Clubs.Any(e => e.Id == id);
+        }
+    }
+}
