cc9f578a4bd41a155fb9950646d756747521d7d5
mfregoso <mfregoso@users.noreply.github.com>
2019-04-24 23:13:39 -0700

    db as cache working, need to add expiration check

--- TCM.Web/Controllers/ClubsController.cs
+++ TCM.Web/Controllers/ClubsController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using TCM.Services;
 using TCM.Services.Utils;
 using System.Collections.Generic;
+using TCM.Models.Domain;
 
 namespace TCM.Web.Controllers
 {
@@ -25,63 +26,81 @@ namespace TCM.Web.Controllers
         [HttpGet]
         public ActionResult<string> Get()
         {
-            string[] monthYear = { "Jan 09", "feb 11", "Sep 18" };
+            //string[] monthYear = { "Jan 09", "feb 11", "Sep 18" };
             //var dates = monthYear.Select(my => DateTime.ParseExact(my, "MMM yy", CultureInfo.InvariantCulture).AddMonths(1).AddDays(-1)).ToArray();
-            var dates = monthYear.Select(day => day.Replace(" ", " 20")).ToArray();
-            var now = DateTime.UtcNow;
-            //System.Diagnostics.Debug.WriteLine(DateHelpers.StaleData(now));
-            //System.Diagnostics.Debug.WriteLine(DateHelpers.StaleData(now.AddDays(-1)));
-            //System.Diagnostics.Debug.WriteLine(DateHelpers.StaleData(now.AddMonths(-1)));
-            return now.ToString();
             return "please get by id: e.g. clubs/455";
         }
 
         [HttpGet("{id}")]
         public ActionResult<ClubInfo> GetByID(string id)
         {
-            bool validId = IdHelpers.IsValid(id);
-            ClubInfo clubInfo = new ClubInfo() { Source = "tm" };
-            if (validId)
+            ClubInfo clubInfo = new ClubInfo();
+            if (!IdHelpers.IsValid(id)) return clubInfo;
+
+            string formattedId = IdHelpers.FormatId(id);
+            var cachedClub = GetClubById(formattedId);
+
+            if (cachedClub != null)
             {
-                string formattedId = IdHelpers.FormatId(id);
-                DateTime tmiExpiration = DateHelpers.GetTmiExpiration();
-
-                clubInfo.Status = ScraperService.GetClubStatus(formattedId);
-
-                Club newClub = new Club() // Entity Framework
-                {
-                    Id = formattedId,
-                    Exists = clubInfo.Status.Exists,
-                    MembershipCount = clubInfo.Status.MembershipCount
-                };
-
-                if (clubInfo.Status.Exists)
-                {
-                    newClub.TMIExpiration = tmiExpiration;
-                    newClub.HistoryExpiration = DateHelpers.GetHistoryExpiration();
-                    clubInfo.History = ScraperService.GetClubPerformance(formattedId);
-
-                    List<MetricsHistory> metricsHistory = new List<MetricsHistory>();
-                    foreach (var item in clubInfo.History)
-                    {
-                        var temp = new MetricsHistory
-                        {
-                            ClubId = formattedId,
-                            Goals = item.Goals,
-                            Members = item.Members,
-                            MonthEnd = item.MonthEnd
-                        };
-                        metricsHistory.Add(temp);
-                    }
-                    newClub.MetricsHistory = metricsHistory;
-                }
-
-                _context.Clubs.Add(newClub);
-                _context.SaveChanges();
-
+                clubInfo.Source = "database";
+                clubInfo.Status = cachedClub;
                 return clubInfo;
             }
-            else return clubInfo;
+
+            DateTime tmiExpiration = DateHelpers.GetTmiExpiration();
+            clubInfo.Status = ScraperService.GetClubStatus(formattedId);
+            clubInfo.Source = "scrape";
+
+            Club newClubEntity = new Club() // Entity Framework
+            {
+                Id = formattedId,
+                Exists = clubInfo.Status.Exists,
+                MembershipCount = clubInfo.Status.MembershipCount
+            };
+
+            if (clubInfo.Status.Exists)
+            {
+                newClubEntity.TMIExpiration = tmiExpiration;
+                newClubEntity.HistoryExpiration = DateHelpers.GetHistoryExpiration();
+                clubInfo.History = ScraperService.GetClubPerformance(formattedId);
+                newClubEntity.MetricsHistory = ConvertHistory(formattedId, clubInfo.History);
+            }
+
+            _context.Clubs.Add(newClubEntity);
+            _context.SaveChanges();
+
+            return clubInfo;
+        }
+
+        private bool ClubExists(string id)
+        {
+            return _context.Clubs.Any(e => e.Id == id);
+        }
+
+        private Club GetClubById(string id)
+        {
+            if (!ClubExists(id)) return null;
+            var club = _context.Clubs.Find(id);
+            club.MetricsHistory = _context.MetricsHistory.Where(row => row.ClubId == id).ToArray();
+            return club;
+        }
+
+        private List<MetricsHistory> ConvertHistory(string ClubId, List<ClubHistory> clubHistory)
+        {
+            if (clubHistory.Count == 0) return null;
+            List<MetricsHistory> metricsHistory = new List<MetricsHistory>();
+            foreach (var item in clubHistory)
+            {
+                var temp = new MetricsHistory
+                {
+                    ClubId = ClubId,
+                    Goals = item.Goals,
+                    Members = item.Members,
+                    MonthEnd = item.MonthEnd
+                };
+                metricsHistory.Add(temp);
+            }
+            return metricsHistory;
         }
     }
 }
