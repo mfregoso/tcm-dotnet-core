bb4a7943569956d6cd820b3a9843448377763423
mfregoso <mfregoso@users.noreply.github.com>
2019-05-07 22:00:10 -0700

    change db connection if in production

--- TCM.Web/Startup.cs
+++ TCM.Web/Startup.cs
@@ -10,25 +10,28 @@ namespace TCM
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
+            InDevelopment = env.IsDevelopment();
         }
 
         public IConfiguration Configuration { get; }
+        public bool InDevelopment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            var db = InDevelopment ? "LocalDb" : "CloudDb";
             services.AddDbContext<ClubDataContext>
-                (options => options.UseSqlServer(Configuration.GetConnectionString("AppDb"), b => b.MigrationsAssembly("TCM.Web")));
+                (options => options.UseSqlServer(Configuration.GetConnectionString(db), b => b.MigrationsAssembly("TCM.Web")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            if (InDevelopment)
             {
                 app.UseDeveloperExceptionPage();
             }
